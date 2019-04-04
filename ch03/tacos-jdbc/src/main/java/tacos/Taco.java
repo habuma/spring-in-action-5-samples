4cb509aea94d6245ffadba067d3ea5739b02a831
Abhishek Ravichandran <aravichandran@tableau.com>
2019-04-04 15:19:22 -0700

    Fix null form ingredients

--- ch03/tacos-jdbc/src/main/java/tacos/Taco.java
+++ ch03/tacos-jdbc/src/main/java/tacos/Taco.java
@@ -21,7 +21,7 @@ public class Taco {
   @Size(min=5, message="Name must be at least 5 characters long")
   private String name;
   
-  @Size(min=1, message="You must choose at least 1 ingredient")
+  @NotNull(message="You must choose at least 1 ingredient")
   private List<Ingredient> ingredients;
 
   /*
